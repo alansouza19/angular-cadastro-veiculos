7ffb87be0f2fdc5a181f16c40f273084f8f3f901
alansouza19 <alan.souza-97@hotmail.com>
2024-01-15 15:58:36 -0300

    Ajustes branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { CadastroVeiculosComponent } from './cadastro-veiculos/cadastro-veiculos.component';
+import { CadastroVeiculosComponent } from './pages/cadastro-veiculos/cadastro-veiculos.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'cadastro-veiculos', component: CadastroVeiculosComponent },
+  { path: 'login', component: LoginComponent },
   
 ];
 
